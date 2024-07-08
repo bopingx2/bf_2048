8547575bef5f002803087ece559f3f9b2800b87e
Boping Xia <60668621+bopingx2@users.noreply.github.com>
2024-07-07 23:14:46 -0400

    Update main.py
    Add comment

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from pygame.locals import *
 
+# Inport logic from utils
 from utils import play
 
 if __name__ == "__main__" :
     pygame.init()
     pygame.display.set_caption('2048')
+    # Call play function to start
     play()
     pygame.quit()
     
